0b1d7d83dca9308efa75678f0cb8b1f0bcb3bb04
Sebastian Wilzbach <seb@wilzba.ch>
2019-08-08 04:31:53 +0200

    Bump SLS to 0.1.18

--- out/bootstrap/sls.js
+++ out/bootstrap/sls.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // the bundled SLS version
-exports.expectedSLSVersion = '0.1.17';
+exports.expectedSLSVersion = '0.1.18';
 //# sourceMappingURL=sls.js.map
